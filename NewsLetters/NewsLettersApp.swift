0239a1e0bc31ba9884c85d3382c937c281c03c07
Gokul Pulikkal <g.gokuldas68@gmail.com>
2025-01-06 16:44:28 -0500

    Initial Commit

---
+++ NewsLetters/NewsLettersApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsLettersApp.swift
+//  NewsLetters
+//
+//  Created by Gokul P on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsLettersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

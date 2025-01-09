680dafdafdad78ccee3712c16078993c86483ab8
Gokul Pulikkal <g.gokuldas68@gmail.com>
2025-01-09 12:56:22 -0500

    structured the launch view

--- NewsLetters/Screens/LaunchScreen/LaunchView.swift
+++ NewsLetters/Screens/LaunchScreen/LaunchView.swift
@@ -8,28 +8,35 @@
 import SwiftUI
 
 struct LaunchView: View {
-    
+
     @State private var viewModel = ViewModel()
 
     var body: some View {
         if viewModel.isLoading {
-            VStack {
-                Image(systemName: "newspaper.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .symbolEffect(.breathe, options: .repeat(.continuous))
-
-                Text("Loading Newsletters...")
-                    .font(.system(size: 25, weight: .heavy))
-                    .foregroundColor(.gray)
-                    .padding(.top)
-            }
-            .task {
-               await viewModel.cacheLatestNewsItems()
-            }
+            splashScreen
+                .task {
+                    await viewModel.cacheLatestNewsItems()
+                }
         } else {
             HomeScreen()
+        }
+    }
+}
+
+extension LaunchView {
+
+    var splashScreen: some View {
+        VStack {
+            Image(systemName: "newspaper.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .symbolEffect(.breathe, options: .repeat(.continuous))
+
+            Text("Loading Newsletters...")
+                .font(.system(size: 25, weight: .heavy))
+                .foregroundColor(.gray)
+                .padding(.top)
         }
     }
 }
